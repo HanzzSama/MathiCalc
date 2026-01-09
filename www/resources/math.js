d9e40a15dd9cdd4629372f4c2d199a22aa848160
HanzzSama <haidarandrianno098@gmail.com>
2026-01-09 10:49:21 +0700

    Update Error = Tak terdifinisi

--- www/resources/math.js
+++ www/resources/math.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
       // ✅ Hasil dibulatkan 8 digit
       return parseFloat(result.toFixed(8));
     } catch (e) {
-      return "Error";
+      return "tak terdefinisi";
     }
   }
 
